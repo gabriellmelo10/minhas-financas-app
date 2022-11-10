d553e810d2aee3acfe27284683dbbb4aef5a2b65
Gabriel Melo <gabriellmelo@gmail.com>
2022-11-10 16:30:33 -0300

    Ajuste na identação do arquivo custom.css

--- src/views/login.js
+++ src/views/login.js
@@ -36,12 +36,12 @@ class Login extends React.Component {
                                                         placeholder="Digite o Email" />
                                                 </FormGroup>
                                                 <FormGroup label="Senha: *" htmlFor="exampleInputPassword1">
-                                                <input type="password"
-                                                    value={this.state.senha}
-                                                    onChange={e => this.setState({senha: e.target.value})}                                                 
-                                                    className="form-control" 
-                                                    id="exampleInputPassword1" 
-                                                    placeholder="Password" />
+                                                    <input type="password"
+                                                        value={this.state.senha}
+                                                        onChange={e => this.setState({senha: e.target.value})}                                                 
+                                                        className="form-control" 
+                                                        id="exampleInputPassword1" 
+                                                        placeholder="Password" />
                                                 </FormGroup>
                                                 <button onClick={this.entrar} className="btn btn-success">Entrar</button>
                                                 <button  className="btn btn-danger">Cadastrar</button>
