06ca5c889ab1a2a4264d9131ebc659b9921f8535
Gabriel Melo <gabriellmelo@gmail.com>
2022-11-14 17:00:09 -0300

    Navegando pelas views através dos componentes

--- src/components/card.js
+++ src/components/card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 function Card(props) {
-  
+
     return (
         <div className='card mb-3'>
-            <h3 className='card-header'>{ props.title }</h3>
+            <h3 className='card-header'>{props.title}</h3>
             <div className='card-body'>
-                { props.children }
+                {props.children}
             </div>
         </div>
     );
---
+++ src/views/cadastroUsuario.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+import Card from '../components/card';
+import FormGroup from '../components/form-group';
+import { withRouter } from 'react-router-dom'
+
+class CadastroUsuario extends React.Component {
+
+    state = {
+        nome: '',
+        email: '',
+        senha: '',
+        senhaRepeticao: ''
+    }
+
+    cadastrar = () => {
+        console.log(this.state)
+    }
+
+    cancelar = () => {
+        this.props.history.push('/login')
+    }
+
+    render() {
+        return (
+            <Card title="Cadastro de Usuário">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="bs-component">
+                            <FormGroup label="Nome: *" htmlFor="inputNome">
+                                <input type="text"
+                                    id="inputNome"
+                                    className="form-control"
+                                    name="nome"
+                                    onChange={e => this.setState({ nome: e.target.value })} />
+                            </FormGroup>
+                            <FormGroup label="Email: *" htmlFor="inputEmail">
+                                <input type="email"
+                                    id="inputEmail"
+                                    className="form-control"
+                                    name="email"
+                                    onChange={e => this.setState({ email: e.target.value })} />
+                            </FormGroup>
+                            <FormGroup label="Senha: *" htmlFor="inputSenha">
+                                <input type="password"
+                                    id="inputSenha"
+                                    className="form-control"
+                                    name="senha"
+                                    onChange={e => this.setState({ senha: e.target.value })} />
+                            </FormGroup>
+                            <FormGroup label="Senha: *" htmlFor="inputRepitaSenha">
+                                <input type="password"
+                                    id="inputRepitaSenha"
+                                    className="form-control"
+                                    name="senha"
+                                    onChange={e => this.setState({ senhaRepeticao: e.target.value })} />
+                            </FormGroup>
+                            <button onClick={this.cadastrar} type="button" className="btn btn-success">Salvar</button>
+                            <button onClick={this.cancelar} type="button" className="btn btn-danger">Cancelar</button>
+                        </div>
+                    </div>
+                </div>
+            </Card>
+        )
+    }
+}
+
+export default withRouter ( CadastroUsuario )
