9813eba009113dff2f1dc124ff7ea0cac522eb27
Gabriel Melo <93332060+gabriellmelo10@users.noreply.github.com>
2022-11-15 18:45:10 -0300

    Criado a página Home

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -16,7 +16,7 @@ function Navbar() {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarResponsive">
                     <ul className="navbar-nav">
-                        <NavBarItem href="#/" label="Home" />
+                        <NavBarItem href="#/home" label="Home" />
                         <NavBarItem href="#/cadastro-usuarios" label="Usuários" />
                         <NavBarItem href="#/" label="Lançamentos" />
                         <NavBarItem href="#/login" label="Login" />
--- src/main/rotas.js
+++ src/main/rotas.js
@@ -4,11 +4,13 @@ import { Route, Switch, HashRouter } from 'react-router-dom'
 
 import CadastroUsuario from '../views/cadastroUsuario';
 import Login from '../views/login';
+import Home from '../views/home';
 
 function Rotas() {
     return (
         <HashRouter>
             <Switch>
+                <Route path="/home" component={Home} />
                 <Route path="/login" component={Login} />
                 <Route path="/cadastro-usuarios" component={CadastroUsuario} />
             </Switch>
