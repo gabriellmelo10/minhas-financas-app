72e6f65141785f6de6765e4411db9c90ddb53b45
Gabriel Melo <93332060+gabriellmelo10@users.noreply.github.com>
2022-11-16 20:46:20 -0300

    Habilitando o CORS e Conectando o Login com a API REST

--- src/views/login.js
+++ src/views/login.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Card from '../components/card'
-import FormGroup from '../components/form-group';
+import FormGroup from '../components/form-group'
 import { withRouter } from 'react-router-dom'
+import axios from 'axios'
 
 
-class Login extends React.Component { 
+class Login extends React.Component {
 
     state = {
         email: '',
@@ -12,18 +13,25 @@ class Login extends React.Component {
     }
 
     entrar = () => {
-        console.log('Email: ', this.state.email)
-        console.log('Senha', this.state.senha)
+        axios
+            .post('http://localhost:8080/api/usuarios/autenticar', {
+                email: this.state.email,
+                senha: this.state.senha
+            }).then( response => {
+                console.log(response)
+            }).catch( erro => {
+                console.log(erro.response)
+            })
     }
 
     prepareCadastrar = () => {
         this.props.history.push('/cadastro-usuarios')
     }
 
-    render(){
+    render() {
         return (
             <div className="row">
-                <div className="col-md-6" style={ {position : 'relative', left: '300px'} }>
+                <div className="col-md-6" style={{ position: 'relative', left: '300px' }}>
                     <div className="bs-docs-section">
                         <Card title="Login">
                             <div className="row">
@@ -33,22 +41,22 @@ class Login extends React.Component {
                                             <FormGroup label="Email: *" htmlFor="exampleInputEmail1">
                                                 <input type="email"
                                                     value={this.state.email}
-                                                    onChange={e => this.setState({email: e.target.value})}
-                                                    className="form-control" 
-                                                    id="exampleInputEmail1" 
-                                                    aria-describedby="emailHelp" 
+                                                    onChange={e => this.setState({ email: e.target.value })}
+                                                    className="form-control"
+                                                    id="exampleInputEmail1"
+                                                    aria-describedby="emailHelp"
                                                     placeholder="Digite o Email" />
                                             </FormGroup>
                                             <FormGroup label="Senha: *" htmlFor="exampleInputPassword1">
                                                 <input type="password"
                                                     value={this.state.senha}
-                                                    onChange={e => this.setState({senha: e.target.value})}                                                 
-                                                    className="form-control" 
-                                                    id="exampleInputPassword1" 
+                                                    onChange={e => this.setState({ senha: e.target.value })}
+                                                    className="form-control"
+                                                    id="exampleInputPassword1"
                                                     placeholder="Password" />
                                             </FormGroup>
                                             <button onClick={this.entrar} className="btn btn-success">Entrar</button>
-                                            <button onClick={this.prepareCadastrar}  className="btn btn-danger">Cadastrar</button>
+                                            <button onClick={this.prepareCadastrar} className="btn btn-danger">Cadastrar</button>
                                         </fieldset>
                                     </div>
                                 </div>
@@ -62,4 +70,4 @@ class Login extends React.Component {
     };
 }
 
-export default withRouter ( Login )
+export default withRouter(Login)
